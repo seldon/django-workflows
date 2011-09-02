695f316041f05e2d346a7b925be9e46e1c3e4b45
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2011-09-02 16:20:24 +0200

    Fixed a typo.

---
+++ workflows/tests/settings.py
@@ -0,0 +1,5 @@
+INSTALLED_APPS = (
+    'workflows',
+    'workflows.tests',
+     # other dependencies go here 
+)
--- workflows/tests/tests.py
+++
@@ -1,5 +0,0 @@
-INSTALLED_APPS = (
-    'workflows',
-    'workflows.tests',
-     # other dependencies go here 
-)
